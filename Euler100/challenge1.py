265c89e45fd295ffac9a9ed8cf6a91da13ae814e
kiberleab Tadesse <kiberleab@users.noreply.github.com>
2020-02-14 18:19:39 +0300

    Update challenge1.py

--- Euler100/challenge1.py
+++ Euler100/challenge1.py
@@ -1,6 +1,10 @@
+# Multiples of 3 and 5
+
+# Find the sum of all the multiples of 3 or 5
+
 def multiplesOf3and5(num):
-  nums = []
-  for n in range(num):
-    if n%5 == 0 or n%3 == 0:
-      nums.append(n)
-  return sum(nums)
+	nums = []
+	for n in range(num):
+		if n%5 == 0 or n%3 == 0:
+			nums.append(n)
+	return sum(nums)
