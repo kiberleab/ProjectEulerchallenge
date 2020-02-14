60f12939a8eb1411206a9e31bbb70b2df7ee0cb7
kiberleab Tadesse <kiberleab@users.noreply.github.com>
2020-02-14 18:22:11 +0300

    Update challenge2.py

--- Euler100/challenge2.py
+++ Euler100/challenge2.py
@@ -3,8 +3,8 @@
 # Find the sum of the even-valued terms.
 
 def fiboEvenSum(num):
-  f = [1,2]
-  while f[-1] < num:
-    f.append(f[-1] + f[-2])
-  fSum = sum(n for n in f[:-1] if n%2==0)
-  return fSum
+	f = [1,2]
+	while f[-1] < num:
+		f.append(f[-1] + f[-2])
+	fSum = sum(n for n in f[:-1] if n%2==0)
+	return fSum
