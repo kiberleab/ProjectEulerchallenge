d6f4d9d15c1df4eb7f35fc37490c5742cea547e3
kiberleab Tadesse <kiberleab@users.noreply.github.com>
2020-02-27 21:10:24 +0300

    Update challenge4.py

--- Euler100/challenge4.py
+++ Euler100/challenge4.py
@@ -1,3 +1,13 @@
 # Largest palindrome product
 
 # Find the largest palindrome made from the product of two 3-digit numbers
+
+def largestPalindromeProduct(n):
+	nums = []
+	upper = 10**n
+	lower = 10**(n-1)
+	for i in range(lower, upper):
+		for j in range(lower, upper):
+			if i*j == int(str(i*j)[::-1]):
+				nums.append(i*j)
+	return max(nums)
